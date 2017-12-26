999423d5bd7fc20e23404a07f4f28909155415f2
Théo Zimmermann <theo.zimmermann@univ-paris-diderot.fr>
2017-12-26 18:30:01 +0100

    Pre-push to another branch.
    Without this GitHub rejects the status check because
    it does not know the commit.

--- certify-origin.sh
+++ certify-origin.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+REMOTE="$1"
+
 # Copy these file to .git/hooks/pre-push and fill these values
 COMMIT_PUSHER=
 GITHUB_STATUS_TOKEN=
@@ -8,6 +10,8 @@ GITHUB_REPO=
 
 while read local_ref COMMIT_SHA remote_ref remote_sha
 do
+
+git push --no-verify $REMOTE +$COMMIT_SHA:refs/heads/tmp
 
 curl -u $COMMIT_PUSHER:$GITHUB_STATUS_TOKEN https://api.github.com/repos/$GITHUB_OWNER/$GITHUB_REPO/statuses/$COMMIT_SHA -d "{\"state\": \"success\", \"target_url\": \"https://github.com/$GITHUB_OWNER/$GITHUB_REPO/commits\", \"description\": \"This commit was pushed by @$COMMIT_PUSHER.\", \"context\": \"certified-origin/$COMMIT_PUSHER\"}"
 
