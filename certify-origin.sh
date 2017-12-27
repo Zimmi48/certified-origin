a7e70e54e66b39a0bbe88ff8ed048cda4caebdfa
Théo Zimmermann <theo.zimmermann@univ-paris-diderot.fr>
2017-12-27 01:09:59 +0100

    Push reference without creating a new branch. The initial push is rejected but creates the corresponding reference on the remote, allowing the status check to be sent.

--- certify-origin.sh
+++ certify-origin.sh
@@ -8,13 +8,11 @@ GITHUB_STATUS_TOKEN=
 GITHUB_OWNER=
 GITHUB_REPO=
 
-while read local_ref COMMIT_SHA remote_ref remote_sha
+while read local_ref COMMIT_SHA REMOTE_REF remote_sha
 do
 
-git push --no-verify $REMOTE +$COMMIT_SHA:refs/heads/tmp
+git push --no-verify $REMOTE $COMMIT_SHA:$REMOTE_REF
 
 curl -u $COMMIT_PUSHER:$GITHUB_STATUS_TOKEN https://api.github.com/repos/$GITHUB_OWNER/$GITHUB_REPO/statuses/$COMMIT_SHA -d "{\"state\": \"success\", \"target_url\": \"https://github.com/$GITHUB_OWNER/$GITHUB_REPO/commits\", \"description\": \"This commit was pushed by @$COMMIT_PUSHER.\", \"context\": \"certified-origin/$COMMIT_PUSHER\"}"
-
-git push --no-verify $REMOTE -d tmp
 
 done
