eecc1c5885344a874bee2c8fc5717eaf0e9bb736
Théo Zimmermann <theo.zimmermann@univ-paris-diderot.fr>
2017-12-27 01:06:28 +0100

    Clean-up temporary branch.

--- certify-origin.sh
+++ certify-origin.sh
@@ -15,4 +15,6 @@ git push --no-verify $REMOTE +$COMMIT_SHA:refs/heads/tmp
 
 curl -u $COMMIT_PUSHER:$GITHUB_STATUS_TOKEN https://api.github.com/repos/$GITHUB_OWNER/$GITHUB_REPO/statuses/$COMMIT_SHA -d "{\"state\": \"success\", \"target_url\": \"https://github.com/$GITHUB_OWNER/$GITHUB_REPO/commits\", \"description\": \"This commit was pushed by @$COMMIT_PUSHER.\", \"context\": \"certified-origin/$COMMIT_PUSHER\"}"
 
+git push --no-verify $REMOTE -d tmp
+
 done
